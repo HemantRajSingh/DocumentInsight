70f487548b0eb41fa31a1f3e91159cc6e8a2cad6
Hemant Raj Singh <rajhemant84@gmail.com>
2024-02-13 23:35:23 -0500

    split and segment the pdf

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,6 +1,7 @@
 import {Pinecone} from "@pinecone-database/pinecone"
 import { downloadFromS3 } from "./s3-server";
 import {PDFLoader} from 'langchain/document_loaders/fs/pdf'
+import {Document, RecursiveCharacterTextSplitter} from '@pinecone-database/doc-splitter'
 
 let pinecone: Pinecone | null = null;
 
@@ -13,13 +14,51 @@ export const getPinecone = async () => {
     return pinecone
 }
 
+type PDFPage = {
+    pageContent: string
+    metadata: {
+        loc: {pageNumber: number}
+    }
+}
+
 export async function loadS3IntoPinecone(fileKey: string) {
+
+    // obtain the pdf, download it from S3 and load pdf
     console.log('downloading S3 into file system')
     const file_name = await downloadFromS3(fileKey)
     if(!file_name) {
         throw new Error('couldn"t download file from S3')
     }
     const loader = new PDFLoader(file_name)
-    const pages = await loader.load()
+    const pages = (await loader.load()) as PDFPage[]
+
+    // split and segment the pdf
+    const documents = await Promise.all(pages.map(page=>prepareDocument))
+    
+    // vectorize and embed individual documents
+
     return pages
+}
+
+export const truncateStringByBytes = (str: string, bytes: number) => {
+    const enc = new TextEncoder()
+    return new TextDecoder('utf-8').decode(enc.encode(str).slice(0, bytes))
+}
+
+async function prepareDocument(page: PDFPage) {
+    let { pageContent, metadata } = page;
+    pageContent = pageContent.replace(/\n/g, '');
+
+    // split the docs
+    const splitter = new RecursiveCharacterTextSplitter();
+    const docs = await splitter.splitDocuments([
+        new Document({
+            pageContent,
+            metadata: {
+                pageNumber: metadata.loc.pageNumber,
+                text: truncateStringByBytes(pageContent, 36000),
+            },
+        }),
+    ]);
+    return docs
 }
